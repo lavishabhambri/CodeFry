7eb04962054db32058e2fd6f11a205e3042d3cb9
vaibhavpathak999 <vaibhavpathak999@gmail.com>
2021-07-24 20:03:05 +0530

    Add Erase Feature

---
+++ CodeFry-Video-Meet/src/Drawing-Board/Board/board.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import "./../Board/board.css"
+
+
+class Board extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount(){
+        this.drawOnCanvas();
+    }
+
+    componentWillReceiveProps(newProps){
+        this.ctx.strokeStyle = newProps.color;
+        this.ctx.lineWidth = newProps.size;
+    }
+
+    drawOnCanvas(){
+        var canvas = document.querySelector('#board');
+        this.ctx = canvas.getContext('2d');
+        var ctx = this.ctx;
+
+        var sketch = document.querySelector('#sketch');
+        var sketch_style = getComputedStyle(sketch);
+        canvas.width = parseInt(sketch_style.getPropertyValue('width'));
+        canvas.height = parseInt(sketch_style.getPropertyValue('height'));
+
+        var mouse = {x: 0, y: 0};
+        var last_mouse = {x: 0, y: 0};
+
+        /* Mouse Capturing Work */
+        canvas.addEventListener('mousemove', function(e) {
+            last_mouse.x = mouse.x;
+            last_mouse.y = mouse.y;
+
+            mouse.x = e.pageX - this.offsetLeft;
+            mouse.y = e.pageY - this.offsetTop;
+        }, false);
+
+
+        /* Drawing on Paint App */
+        ctx.lineWidth = this.props.size;
+        ctx.lineJoin = 'round';
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = this.props.color;
+
+        canvas.addEventListener('mousedown', function(e) {
+            canvas.addEventListener('mousemove', onPaint, false);
+        }, false);
+
+        canvas.addEventListener('mouseup', function() {
+            canvas.removeEventListener('mousemove', onPaint, false);
+        }, false);
+        
+        var root = this;
+        var onPaint = function() {
+            ctx.beginPath();
+            ctx.moveTo(last_mouse.x, last_mouse.y);
+            ctx.lineTo(mouse.x, mouse.y);
+            ctx.closePath();
+            ctx.stroke();
+        };
+
+    }
+
+    render(){
+        return(
+            <div id="sketch" className="sketch">
+                <canvas className="board" id="board">
+
+                </canvas>
+            </div>
+        )
+    }
+}
+
+export default Board;
